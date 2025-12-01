Corrected GoogLeNet's issues with Guided Backprop and Guided Grad-CAM and added the prediction label to the name of the images generated by the explainable methods

utils/explainable_methods.py
@@ -7,10 +7,11 @@ from imagenet_classes import classes
 from pytorch_grad_cam.utils.image import deprocess_image, show_cam_on_image
 from pytorch_grad_cam import GradCAM, GuidedBackpropReLUModel
 from utils.classifier_output_targets import ClassifierOutputTargets
+from utils.counterfactual_gradcam import CounterfactualGradCAM    
 
 def run_gradcam(filename:str, targets:List[ClassifierOutputTargets],
                 input_tensor:torch.Tensor, rgb_img:np.ndarray, 
-                output_folder_prefix:str, grad_cam:GradCAM):
+                output_folder_prefix:str, grad_cam:GradCAM, prediction:int):
     # Generate heatmp from Grad-CAM
     grad_cam_vis = grad_cam(input_tensor=input_tensor, targets=targets)[0, :]
 
@@ -18,8 +19,8 @@ def run_gradcam(filename:str, targets:List[ClassifierOutputTargets],
     gradcam_img = show_cam_on_image(rgb_img, grad_cam_vis, use_rgb=True)
 
     # Save grad-cam img
-    if(targets is None) :
-        classes_labels = "predictedclass"
+    if(targets is None) : # label will be predicted class
+        classes_labels = classes[prediction]
     elif isinstance(targets[0].categories, List):
         delimeter = "_"
         categories_id = targets[0].categories
@@ -27,29 +28,51 @@ def run_gradcam(filename:str, targets:List[ClassifierOutputTargets],
     else:
         classes_labels = classes[targets[0].categories]
     save_folder = os.path.join(output_folder_prefix, "grad-cam")
-    save_path =  os.path.join(save_folder, f"{classes_labels}_{filename}")
+    os.makedirs(save_folder, exist_ok=True)
+    save_path =  os.path.join(save_folder, f"{classes_labels};{filename}")
     cv2.imwrite(save_path, gradcam_img[:, :, ::-1])
     
+def run_counterfactual_gradcam(filename:str, targets:List[ClassifierOutputTargets],
+                input_tensor:torch.Tensor, rgb_img:np.ndarray, 
+                output_folder_prefix:str, counterfactual_grad_cam:CounterfactualGradCAM, prediction:int):
+    # Generate heatmp from Grad-CAM
+    counterfactual_grad_cam_vis = counterfactual_grad_cam(input_tensor=input_tensor, targets=targets)[0, :]
+
+    # Overlay heatmap in image
+    gradcam_img = show_cam_on_image(rgb_img, counterfactual_grad_cam_vis, use_rgb=True)
+
+    # Save grad-cam img
+    if(targets is None): # label will be predicted class
+        classes_labels = classes[prediction]
+    elif isinstance(targets[0].categories, List):
+        delimeter = "_"
+        categories_id = targets[0].categories
+        classes_labels = delimeter.join([classes[i] for i in categories_id])
+    else:
+        classes_labels = classes[targets[0].categories]
+    save_folder = os.path.join(output_folder_prefix, "counterfactual_explanations")
+    os.makedirs(save_folder, exist_ok=True)
+    save_path =  os.path.join(save_folder, f"{classes_labels};{filename}")
+    cv2.imwrite(save_path, gradcam_img[:, :, ::-1])
 
 def run_guided_backprop(filename:str, target:int, 
                         input_tensor:torch.Tensor, output_folder_prefix:str, 
-                        guided_backprop:GuidedBackpropReLUModel):
-    
+                        guided_backprop:GuidedBackpropReLUModel, prediction:int):
     guided_backprop_vis = guided_backprop(input_tensor, target_category=target)
     guided_backprop_img = deprocess_image(guided_backprop_vis)
-
     # Save guided_backprop img
-    if(target is None):
-        class_label = "predictedclass"
+    if(target is None): # label will be predicted class
+        class_label = classes[prediction]
     else:
         class_label = classes[target]
     save_folder = os.path.join(output_folder_prefix, f"guided_backprop")
-    save_path = os.path.join(save_folder, f"{class_label}_{filename}")
+    os.makedirs(save_folder, exist_ok=True)
+    save_path = os.path.join(save_folder, f"{class_label};{filename}")
     cv2.imwrite(save_path, guided_backprop_img[:, :, ::-1])
 
 def run_guided_gradcam(filename:str, target:Optional[int], 
                     input_tensor:torch.Tensor, output_folder_prefix:str, 
-                    guided_backprop:GuidedBackpropReLUModel, grad_cam:GradCAM):
+                    guided_backprop:GuidedBackpropReLUModel, grad_cam:GradCAM, prediction:int):
             
     # Get guided backprop visualization
     guided_backprop_vis = guided_backprop(input_tensor, target_category=target)
@@ -63,11 +86,12 @@ def run_guided_gradcam(filename:str, target:Optional[int],
     guided_gradcam_img = deprocess_image(guided_backprop_vis * grad_cam_vis)
 
     # Save guided_backprop img
-    if(target is None):
-        class_label = "predictedclass"
+    if(target is None): # label will be predicted class
+        class_label = classes[prediction]
     else:
         class_label = classes[target]
     save_folder = os.path.join(output_folder_prefix, f"guided_grad-cam")
-    save_path = os.path.join(save_folder, f"{class_label}_{filename}")
+    os.makedirs(save_folder, exist_ok=True)
+    save_path = os.path.join(save_folder, f"{class_label};{filename}")
     cv2.imwrite(save_path, guided_gradcam_img[:, :, ::-1])
 