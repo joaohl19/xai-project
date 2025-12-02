3bd6846d123d1b7487ab8581ad73a8818c037fa4
João Henrique <joaohl19@users.noreply.github.com>
2025-12-02 16:43:19 -0300

    Added examples folder that should contain the images used to test the biased model and added .pth file of the pretrained biased model

--- biased_model/training.py
+++ biased_model/training.py
@@ -1,7 +1,12 @@
+import torch
+import numpy as np
+from torch import nn
+from model import SimpleCNN
+import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-# Images preprocessing
+# Image preprocessing
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
@@ -9,9 +14,50 @@ transform = transforms.Compose([
 ])
 
 # Loading data based on directory structure and names
-dataset = datasets.ImageFolder(root='dataset/train', transform=transform)
+train_dataset = datasets.ImageFolder(root='dataset/train', transform=transform)
 
-print(dataset.class_to_idx) 
-# {'dog': 0, 'cat': 1}
+print(train_dataset.class_to_idx) 
+# {'cat': 0, 'dog': 1}
 
-dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+# Instantiating the model and configuring the parameters
+model = SimpleCNN(dropout_rate=0.3)
+num_epochs = 10
+learning_rate = 0.001
+batch_size = 32
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("Using GPU")
+else:
+    device = torch.device("cpu")
+    print("Using CPU")
+
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+# Training the model
+model.to(device)
+model.train()
+for epoch in range(num_epochs):
+    train_losses = []
+    train_avg_losses = []
+    for X_batch, y_batch in train_dataloader:
+        X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+
+        optimizer.zero_grad()
+
+        outputs = model.forward(X_batch)
+      
+        loss = loss_function(outputs, y_batch.view(-1))
+
+        loss.backward()
+
+        optimizer.step()
+
+        train_losses.append(loss.item())
+
+    train_avg_loss = np.mean(train_losses)
+    train_avg_losses.append(train_avg_loss)
+    print(f'Epoch#{epoch+1}: Train Average Loss = {train_avg_loss:.5f}')
+
+torch.save(model.state_dict(), "pretrained_model.pth")
