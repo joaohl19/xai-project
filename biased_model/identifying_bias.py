6a5207ad2aba898534aa5e5ebfaf81a5014a1ace
João Henrique <joaohl19@users.noreply.github.com>
2025-12-04 15:49:43 -0300

    Added some test examples for identifying bias in the model and refactored identifying_bias.py

--- biased_model/identifying_bias.py
+++ biased_model/identifying_bias.py
@@ -43,21 +43,29 @@ target_layers = [model.conv4]
 grad_cam = GradCAM(model=model, target_layers=target_layers) 
 
 # Testing the biased model for cat images
-path_cat = "dataset/test/cat"
-directory = os.fsencode(path_cat)
+path = "dataset/test/cat"
+path_save = "examples/cat"
+os.makedirs(path_save, exist_ok=True)
+directory = os.fsencode(path)
+
+# Defining the target of Grad-CAM
+target_idx = class_to_idx['cat']
+target_class = idx_to_class[target_idx]
+
+print(f"Evaluating {target_class} images")
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
 
     # Reading the image and resizing it to fit the model's architecture
-    rgb_img = Image.open(os.path.join(path_cat, filename)).convert('RGB')
+    rgb_img = Image.open(os.path.join(path, filename)).convert('RGB')
     rgb_img = rgb_img.resize((224,224), Image.LANCZOS)
 
     # Pre-processing the image
     input_tensor = transform(rgb_img).unsqueeze(0).to(device)
 
-    # Generate heatmp from Grad-CAM
-    target= [ClassifierOutputTarget(class_to_idx['cat'])]
-    grad_cam_vis = grad_cam(input_tensor=input_tensor, targets=None)[0, :]
+    # Generate heatmp from Grad-CAM for target class
+    target = [ClassifierOutputTarget(target_idx)]
+    grad_cam_vis = grad_cam(input_tensor=input_tensor, targets=target)[0, :]
     
     numpy_img = asarray(rgb_img) / 255
     
@@ -69,8 +77,49 @@ for file in os.listdir(directory):
     probs = F.softmax(output, dim=1).squeeze(0)
 
     # Save the grad_cam image
-    new_filename = "test"+filename
-    cv2.imwrite(os.path.join(path_cat, new_filename), gradcam_img[:, :, ::-1])
+    new_filename = "target_"+target_class+filename
+    cv2.imwrite(os.path.join(path_save, new_filename), gradcam_img[:, :, ::-1])
+    
+    for idx in range(len(probs)):
+        print(f"{filename} prediction: {idx_to_class[idx]} with {probs[idx].item() * 100 :.2f}% of confidence")
+
+# Testing the biased model for cat images
+path = "dataset/test/dog"
+path_save = "examples/dog"
+os.makedirs(path_save, exist_ok=True)
+directory = os.fsencode(path)
+
+# Defining the target of Grad-CAM
+target_idx = class_to_idx['dog']
+target_class = idx_to_class[target_idx]
+
+print(f"Evaluating {target_class} images")
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+
+    # Reading the image and resizing it to fit the model's architecture
+    rgb_img = Image.open(os.path.join(path, filename)).convert('RGB')
+    rgb_img = rgb_img.resize((224,224), Image.LANCZOS)
+
+    # Pre-processing the image
+    input_tensor = transform(rgb_img).unsqueeze(0).to(device)
+
+    # Generate heatmp from Grad-CAM for target class
+    target = [ClassifierOutputTarget(target_idx)]
+    grad_cam_vis = grad_cam(input_tensor=input_tensor, targets=target)[0, :]
+    
+    numpy_img = asarray(rgb_img) / 255
+    
+    # Overlay heatmap in image
+    gradcam_img = show_cam_on_image(numpy_img, grad_cam_vis, use_rgb=True)
+    
+    # Get probabilities of the output
+    output = model(input_tensor)
+    probs = F.softmax(output, dim=1).squeeze(0)
+
+    # Save the grad_cam image
+    new_filename = "target_"+target_class+filename
+    cv2.imwrite(os.path.join(path_save, new_filename), gradcam_img[:, :, ::-1])
     
     for idx in range(len(probs)):
         print(f"{filename} prediction: {idx_to_class[idx]} with {probs[idx].item() * 100 :.2f}% of confidence")
